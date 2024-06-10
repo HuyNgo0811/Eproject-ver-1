a47ec32d5f9ad6776bd2c328ba6823b6f51ab7a8
HuyNgo0811 <128045827+HuyNgo0811@users.noreply.github.com>
2024-06-10 15:55:30 +0700

    create login controller

---
+++ Eproject-ver-1/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eproject_ver_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet("{EmployeeId}/{password}")]
+        public async Task<bool> GetEmployee(string EmployeeId, string password)
+        {
+            var account = await db.Employees.SingleOrDefaultAsync(e => e.EmployeeId == EmployeeId && e.Password == password);
+            if (account == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Eproject-ver-1/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿namespace Eproject_ver_1.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int role_id { get; set; }
+        public string Permition { get; set; }
+        public string Address { get; set; }
+        public string Status { get; set; }
+        public string Deleted { get; set; }
+
+    }
+}
